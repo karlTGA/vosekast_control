1a1fbd5216266e5f606f80d9690f87dbda9b342b
Karl <karlwolffgang@googlemail.com>
2020-03-26 15:58:37 +0100

    Rename HomeView to OverView

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import { Layout } from "antd";
 
-import HomeView from "./components/views/HomeView";
+import OverView from "./components/views/OverView";
 
 export default function Routes() {
   return (
     <Layout>
       <Switch>
-        <Route path="/" component={HomeView} />
+        <Route path="/" component={OverView} />
       </Switch>
     </Layout>
   );
---
+++ frontend/src/components/views/OverView.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const OverView = () => {
+  return <></>;
+};
+
+export default OverView;
