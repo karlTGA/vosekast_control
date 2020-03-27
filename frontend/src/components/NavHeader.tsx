6f50293017f1675773f9e85958d007fe1978397b
Karl <karlwolffgang@googlemail.com>
2020-03-27 12:16:42 +0100

    Refactor Indicators and place them on navbar

--- frontend/src/components/NavHeader.tsx
+++ frontend/src/components/NavHeader.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Layout, Menu } from "antd";
 import { Link } from "react-router-dom";
+import { MQTTOnlineIndicator, VosekastOnlineIndicator } from "./Indicators";
 
 const { Header } = Layout;
 
@@ -17,6 +18,20 @@ export default function NavHeader() {
         </Menu.Item>
         <Menu.Item key="sequences">
           <Link to="/sequences">Test Sequences</Link>
+        </Menu.Item>
+        <Menu.Item
+          key="mqtt_indicator"
+          className="indicator-menu-item"
+          disabled
+        >
+          <MQTTOnlineIndicator />
+        </Menu.Item>
+        <Menu.Item
+          key="vosekast_indicator"
+          className="indicator-menu-item"
+          disabled
+        >
+          <VosekastOnlineIndicator />
         </Menu.Item>
       </Menu>
     </Header>
