7cfb38edaf2fbbedb2b36ebb71568d09a6ee79ea
Karl <karlwolffgang@googlemail.com>
2021-10-07 11:04:11 +0200

    switch to less

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.scss";
+import "./App.css";
 import MainView from "./components/views/MainView";
 import { BrowserRouter as Router } from "react-router-dom";
 
